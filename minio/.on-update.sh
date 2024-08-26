591dcf30548978d9b8728b89a87cb76cf505fcf7
Ozeliurs <ozeliurs@gmail.com>
2024-08-26 20:29:02 +0700

    refactor(.on-update.sh): extract project id to a variable for better maintainability

--- minio/.on-update.sh
+++ minio/.on-update.sh
@@ -1,5 +1,6 @@
 # Update Secrets
-infisical export --format=dotenv --projectId 09b0a40c-0431-4596-a6cb-47e7c6cb767b --domain https://infisical.ozeliurs.com -e prod > .env
+PROJECT_ID="09b0a40c-0431-4596-a6cb-47e7c6cb767b"
+infisical export --format=dotenv --projectId $PROJECT_ID --domain https://infisical.ozeliurs.com -e prod > .env
 
 # Restart all containers
 docker compose up -d --remove-orphans
