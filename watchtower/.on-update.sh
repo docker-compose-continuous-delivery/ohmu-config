5739339c4672d8cbb807613f3427f23750c11570
Ozeliurs <ozeliurs@gmail.com>
2024-08-26 20:32:59 +0700

    feat(watchtower/.on-update.sh): add Infisical secrets update step on container update

--- watchtower/.on-update.sh
+++ watchtower/.on-update.sh
@@ -1,3 +1,7 @@
+# Update Secrets
+PROJECT_ID="8f952ab8-e2c4-4203-b153-3a723bca4702"
+infisical export --format=dotenv --projectId $PROJECT_ID --domain https://infisical.ozeliurs.com -e prod > .env
+
 # Restart all containers
 docker compose up -d --remove-orphans
 
