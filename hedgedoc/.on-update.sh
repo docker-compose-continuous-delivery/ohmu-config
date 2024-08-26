d9cbc83ee902d5b9018fcb0e2684009cce3308ce
Ozeliurs <ozeliurs@gmail.com>
2024-08-27 00:16:52 +0700

    feat(hedgedoc): ensure appdata folders exist before updating

--- hedgedoc/.on-update.sh
+++ hedgedoc/.on-update.sh
@@ -1,3 +1,7 @@
+# Make sure the "bookstack" appdata folders exists
+sudo mkdir -p /mnt/appdata/hedgedoc/app
+sudo mkdir -p /mnt/appdata/hedgedoc/db
+
 # Restart all containers
 docker compose up -d --remove-orphans
 
