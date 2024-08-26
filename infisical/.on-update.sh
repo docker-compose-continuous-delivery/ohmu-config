4772537daf9415113e2bab83fda2be59d6b9b11b
Ozeliurs <ozeliurs@gmail.com>
2024-08-26 20:37:13 +0700

    feat(on-update.sh): generate .env file with encryption key and auth secret if missing

--- infisical/.on-update.sh
+++ infisical/.on-update.sh
@@ -1,3 +1,9 @@
+# If .env is missing, create it
+if [ ! -f .env ]; then
+    echo "ENCRYPTION_KEY=$(openssl rand -hex 16)" > .env
+    echo "AUTH_SECRET=$(openssl rand -base64 32)" >> .env
+fi
+
 # Restart all containers
 docker compose up -d --remove-orphans
 
