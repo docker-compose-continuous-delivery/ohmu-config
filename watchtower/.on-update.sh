eaa440c1fdbac35bd0597c207a50e620a9db391f
Ozeliurs <ozeliurs@gmail.com>
2024-08-26 21:20:43 +0700

    refactor(on-update.sh): improve secrets update process to compare and update .env file only when changes are detected

--- watchtower/.on-update.sh
+++ watchtower/.on-update.sh
@@ -1,6 +1,14 @@
 # Update Secrets
 PROJECT_ID="a054ea85-57a7-4c13-b841-1b432915bda4"
-infisical export --format=dotenv --projectId $PROJECT_ID --domain https://infisical.ozeliurs.com -e prod > .env
+
+# Export secrets and compare directly with the existing .env file
+if ! infisical export --format=dotenv --projectId $PROJECT_ID --domain https://infisical.ozeliurs.com -e prod | cmp -s - .env; then
+    # If different, update the existing .env file
+    infisical export --format=dotenv --projectId $PROJECT_ID --domain https://infisical.ozeliurs.com -e prod > .env
+    echo "Configs updated from Infisical"
+else
+    echo "Configs are identical, no update needed"
+fi
 
 # Restart all containers
 docker compose up -d --remove-orphans
