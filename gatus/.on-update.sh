6a9cda806d56b2eafb8869651c4cc47932211a25
Ozeliurs <ozeliurs@gmail.com>
2024-08-27 22:38:10 +0700

    refactor(.on-update.sh): improve script to only recreate service when monitored files change

--- gatus/.on-update.sh
+++ gatus/.on-update.sh
@@ -15,7 +15,24 @@ fi
 
 ../.scripts/envsubst.sh .env config.tpl.yaml config.yaml
 
-# Restart all containers
-docker compose up -d --remove-orphans
+# List of files to monitor for changes
+FILES_TO_MONITOR=("config.yaml")
+
+# Check if any of the specified files have changed
+CHANGED=false
+for FILE in "${FILES_TO_MONITOR[@]}"; do
+    if git diff --name-only HEAD@{1} HEAD | grep -q "$FILE"; then
+        CHANGED=true
+        break
+    fi
+done
+
+if [ "$CHANGED" = true ]; then
+    echo "One of the monitored files has changed, recreating service"
+    docker compose up -d --force-recreate --remove-orphans
+else
+    echo "No changes detected in monitored files"
+    docker compose up -d --remove-orphans
+fi
 
 echo "Successfully updated!"
