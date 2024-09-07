0baddf0426356c7c413f6404fb6f551386acf506
Ozeliurs <ozeliurs@gmail.com>
2024-09-07 10:59:28 +0200

    feat(update.sh): enhance script with colorized output and improved logging

--- .update.sh
+++ .update.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Define color codes
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
 # Save the parent directory of this script to a variable
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -22,17 +28,17 @@ done
 
 # Fetch the latest changes from the remote if not forcing update
 if [ "$FORCE_UPDATE" = false ]; then
-  echo "Fetching latest changes"
+  echo "${YELLOW}[+] Fetching latest changes${NC}"
   git fetch
 
   # Check if there are any changes
   if [[ $(git rev-parse HEAD) == $(git rev-parse @{u}) ]]; then
-      echo "No changes to pull"
+      echo "${YELLOW}[+] No changes to pull${NC}"
       exit 0
   fi
 
   # Pull the latest changes, overwriting any local changes
-  echo "Pulling latest changes"
+  echo "${YELLOW}[+] Pulling latest changes${NC}"
   git pull -X theirs
 fi
 
@@ -44,10 +50,15 @@ set +a
 echo "Running on-update.sh for each folder ..."
 for f in */; do
     if [ -f "$f.on-update.sh" ]; then
-        echo "Running on-update.sh in $f"
+        echo "${YELLOW}[+] Running on-update.sh in $f${NC}"
         cd $f
         chmod +x .on-update.sh
         ./.on-update.sh
+        if [ $? -eq 0 ]; then
+            echo -e "${GREEN}[+] Update successful in $f${NC}"
+        else
+            echo -e "${RED}[+] Update failed in $f${NC}"
+        fi
         git restore .on-update.sh
         cd ..
     fi
