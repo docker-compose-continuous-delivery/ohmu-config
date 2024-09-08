7f65272994dcdbf1d62403a6d8f9971cc0c8ec7a
Ozeliurs <ozeliurs@gmail.com>
2024-09-08 20:10:05 +0200

    feat(viseron): add configuration and secrets management The configuration is now externalized using a template file and environment variables, allowing for more flexibility and security. The update script has been enhanced to create required folders and update secrets.

--- viseron/.on-update.sh
+++ viseron/.on-update.sh
@@ -1,5 +1,11 @@
+# Make sure the required folders exists
 sudo mkdir -p /mnt/appdata/viseron
 sudo mkdir -p /mnt/data/viseron
+
+# Update secrets
+../.scripts/update_secrets.sh 954bd56a-581e-42c9-a383-bdc9fc9bb262 .env
+
+../.scripts/envsubst.sh .env config.tpl.yaml config.yaml
 
 # Run docker compose up
 docker compose up -d --remove-orphans
