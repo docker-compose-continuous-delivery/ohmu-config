c9be2ee6f77dd50116ebb3cc3c1496ec8cbaa495
Ozeliurs <ozeliurs@gmail.com>
2024-09-08 19:42:06 +0200

    chore(on-update.sh): add permissions configuration for ispy data and appdata directories The script now sets the correct ownership for the ispy data and appdata directories, ensuring that the application has the necessary permissions to function correctly.

--- ispy/.on-update.sh
+++ ispy/.on-update.sh
@@ -1,5 +1,8 @@
 sudo mkdir -p /mnt/data/ispy
 sudo mkdir -p /mnt/appdata/ispy
 
+sudo chown 1000 -R /mnt/data/ispy
+sudo chown 1000 -R /mnt/appdata/ispy
+
 # Restart all containers
 docker compose up -d --remove-orphans
