013d03b011f3ab51c5b4213b7376a65fb96e5ff3
Ozeliurs <ozeliurs@gmail.com>
2024-09-08 23:08:39 +0200

    feat(kavita): add docker compose configuration and on-update script for kavita service This commit introduces a Docker Compose configuration for the Kavita service, which includes volume mounts and Traefik integration. Additionally, an on-update script is added to create the necessary directories and Docker volumes for the service to run properly.

---
+++ kavita/.on-update.sh
@@ -0,0 +1,12 @@
+sudo mkdir -p /mnt/appdata/kavita
+
+# Ensure the books docker volume exists
+sudo mkdir -p /mnt/data/books
+sudo chown -R 1000:1000 /mnt/data/books
+docker volume create --driver local \
+    --opt type=none \
+    --opt device=/mnt/data/books \
+    --opt o=bind \
+    books
+
+docker compose up -d --remove-orphans
