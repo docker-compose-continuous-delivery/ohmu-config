6fbc1782e0b5649f1fa108b17766f1c218450821
Ozeliurs <ozeliurs@gmail.com>
2024-09-18 11:19:28 +0200

    refactor(config): replace hardcoded database credentials with environment variables The hardcoded database credentials have been replaced with environment variables to improve security and flexibility. This change allows for easier management of sensitive data and enables the use of different credentials in different environments.

--- infisical/.on-update.sh
+++ infisical/.on-update.sh
@@ -2,6 +2,7 @@
 if [ ! -f .env ]; then
     echo "ENCRYPTION_KEY=$(openssl rand -hex 16)" > .env
     echo "AUTH_SECRET=$(openssl rand -base64 32)" >> .env
+    echo "POSTGRES_PASSWORD=$(openssl rand -base64 32)" >> .env
 fi
 
 echo "Infisical does not update automatically, please check for updates manually"
