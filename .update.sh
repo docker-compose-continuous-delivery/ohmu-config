65263b42ed126b561e4fc5828a15dbd9d77e939c
Ozeliurs <ozeliurs@gmail.com>
2024-09-30 11:27:11 +0200

    feat(update.sh): add lock file mechanism to prevent concurrent script execution

--- .update.sh
+++ .update.sh
@@ -12,6 +12,21 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Change to the parent of the parent directory of this script
 cd "$SCRIPT_DIR/.." || exit
 
+# Define lock file
+LOCK_FILE="/tmp/update_script.lock"
+
+# Check if the script is already running
+if [ -f "$LOCK_FILE" ]; then
+    echo "${RED}[!] The script is already running${NC}"
+    exit 1
+fi
+
+# Create lock file
+touch "$LOCK_FILE"
+
+# Trap to remove lock file on exit
+trap 'rm -f "$LOCK_FILE"; exit $?' INT TERM EXIT
+
 # Parse command-line arguments
 FORCE_UPDATE=false
 while getopts "f" opt; do
