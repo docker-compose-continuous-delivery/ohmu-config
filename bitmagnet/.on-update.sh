ded8d8dfb91a9b172ffcb4a84df7810db15250f7
Ozeliurs <ozeliurs@gmail.com>
2025-01-04 15:38:54 +0100

    refactor(.on-update.sh): simplify script by removing update secrets step and reducing overall complexity

--- bitmagnet/.on-update.sh
+++ bitmagnet/.on-update.sh
@@ -3,8 +3,5 @@
 # Make sure the "bitmagnet" appdata folders exists
 sudo mkdir -p /mnt/appdata/bitmagnet/postgres
 
-# Update secrets
-../.scripts/update_secrets.sh <unique-uuid> .env
-
 # Restart all containers
 docker compose up -d --remove-orphans
